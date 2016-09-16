9e9aa3942c38d162c1fc0fcc7adc556edf239d71
laure <laure.nicollet@eleve.ensai.fr>
2016-09-16 12:03:56 +0200

    first commit

---
+++ src/main/java/group12/test/App.java
@@ -0,0 +1,12 @@
+package group12.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		// ceci est un commentaire rajoute
+		System.out.println("Hello World!");
+	}
+}
